f4263c323d84f1d7eaf2600766ad2406bc6f94ba
채영준 <157681635+cyjoon68@users.noreply.github.com>
2026-07-08 02:49:36 +0900

    fix(test): add junit platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   runtimeOnly("com.mysql:mysql-connector-j")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
